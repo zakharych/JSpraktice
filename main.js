bad30d4915da24e000384160f56179fa1554a5da
zakharych <alexzlkl@yandex.ru>
2020-07-01 20:23:09 +0300

    3 ex

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//фильтрация строки
+
 
 const string = "Привет! Как дела?";
 var pattern = ["а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е"];
@@ -6,13 +8,50 @@ let extractedVowels = ""
 
 for (let i=0 ; i < stringToFilter.length; i++){
 
-   const curentLettres = stringToFilter[i].toLowerCase;
+   const curentLettres = stringToFilter[i].toLowerCase();
 
         if (pattern.includes(curentLettres)){
-            extractedVowels =+ curentLettres;
+            extractedVowels += curentLettres;
         }
 }
 return extractedVowels;
 }
 
 console.log(getVowels(string));
+
+// Выборка обекта
+
+
+const workers = [
+   {"name":"John","salary":500},
+   {"name":"Mike","salary":1300},
+   {"name":"Linda","salary":1500}];
+
+
+var ar_names = [];
+var getWorthyWorkers = arFilter =>{
+
+for (let i=0 ; i < workers.length; i++){
+  
+        if (arFilter[i].salary > 1000){
+            ar_names.push(workers[i].name);
+        }
+
+}
+return(ar_names)
+};
+console.log(getWorthyWorkers(workers))
+
+
+// Анализ строки
+
+const path = "/users/download/index.html"
+
+var isHtml = string =>{
+   
+   return(path.endsWith(".html"))
+} 
+
+console.log(isHtml(path))
+
+//
